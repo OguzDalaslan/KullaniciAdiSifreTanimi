bc4e0703089e19f333a64f626075a38e3f2fb160
OguzDalaslan <104154904+OguzDalaslan@users.noreply.github.com>
2022-04-24 13:06:28 +0300

    KullaniciGirisi

---
+++ Ahmet,123/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kullaniciadi,sifre;
+            Console.WriteLine("Kullanici Adını Giriniz:");
+            kullaniciadi = Console.ReadLine();
+            Console.WriteLine("Şifreyi Giriniz");
+            sifre=Console.ReadLine();
+            if (kullaniciadi == "Ahmet" && sifre == "123")
+            {
+                Console.WriteLine("Giriş Başarılı");
+            }
+            else
+            {
+                Console.WriteLine("Kullanici Adı veya Şifre Hatalı");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
